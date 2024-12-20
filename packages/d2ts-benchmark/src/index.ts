417596424f4a09a23641ed44e59c065095d767d1
Sam Willis <sam.willis@gmail.com>
2024-12-20 11:31:40 +0000

    Add sqlite join to the benchmark

--- packages/d2ts-benchmark/src/index.ts
+++ packages/d2ts-benchmark/src/index.ts
@@ -1,5 +1,8 @@
 import Benchmark from 'benchmark'
 import { D2, MultiSet, map, join, filter, v } from 'd2ts'
+import { join as joinSql } from 'd2ts/sqlite'
+import { BetterSQLite3Wrapper } from 'd2ts/sqlite'
+import Database from 'better-sqlite3'
 
 // Sample data generation
 const generateData = (size: number) => {
@@ -116,6 +119,59 @@ const joinWithD2TS = () => {
   return joined
 }
 
+// SQLite-based D2TS implementation
+const joinWithD2TSAndSQLite = () => {
+  const sqlite = new Database(':memory:')
+  const db = new BetterSQLite3Wrapper(sqlite)
+  
+  const graph = new D2({ initialFrontier: v([0]) })
+  const usersStream = graph.newInput<[number, (typeof allUsers)[0]]>()
+  const postsStream = graph.newInput<[number, (typeof allPosts)[0]]>()
+
+  const joined = usersStream.pipe(
+    joinSql(postsStream, db),
+    map(
+      ([_key, [user, post]]: [
+        number,
+        [(typeof allUsers)[0], (typeof allPosts)[0]],
+      ]) => ({
+        userName: user.name,
+        postTitle: post.title,
+      }),
+    ),
+  )
+
+  graph.finalize()
+
+  // Send initial data
+  usersStream.sendData(v([1]), initialUsersSet)
+  postsStream.sendData(v([1]), initialPostsSet)
+  graph.step()
+
+  // Incrementally add remaining items
+  for (let i = 0; i < incrementalUsers.length; i++) {
+    const user = incrementalUsers[i]
+    const post1 = incrementalPosts[i * 2]
+    const post2 = incrementalPosts[i * 2 + 1]
+
+    usersStream.sendData(
+      v([i + 2]),
+      new MultiSet([[[user.id, user] as [number, (typeof allUsers)[0]], 1]]),
+    )
+    postsStream.sendData(
+      v([i + 2]),
+      new MultiSet([
+        [[post1.userId, post1] as [number, (typeof allPosts)[0]], 1],
+        [[post2.userId, post2] as [number, (typeof allPosts)[0]], 1],
+      ]),
+    )
+    graph.step()
+  }
+
+  db.close()
+  return joined
+}
+
 // Add tests
 joinSuite
   .add('Naive Join', () => {
@@ -123,6 +179,9 @@ joinSuite
   })
   .add('D2TS Join', () => {
     joinWithD2TS()
+  })
+  .add('D2TS SQLite Join', () => {
+    joinWithD2TSAndSQLite()
   })
   .on('cycle', (event: Benchmark.Event) => {
     console.log(String(event.target))
